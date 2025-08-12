8ee147b58fddb9ab97b8e98b42dddf93dcf2d943
MayaraOliveir <mayaraoliveir00@gmail.com>
2025-08-12 16:24:43 -0300

    Desafio de projeto concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Nokia nokia = new Nokia("8599446611", "Nokia SE", "123987654", 128);
+
+
+Iphone iphone = new Iphone("8599332211", "Iphone 12 PRO", "123654987", 256);
+
+
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Spotify");
